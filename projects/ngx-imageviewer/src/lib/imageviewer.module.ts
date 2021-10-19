6e9c81a60a43445556776cb25848fe0923aa5635
Max van der Velde <max@klippa.com>
2021-10-19 13:58:40 +0200

    Fixed panning delay

--- projects/ngx-imageviewer/src/lib/imageviewer.module.ts
+++ projects/ngx-imageviewer/src/lib/imageviewer.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { ImageViewerComponent } from './imageviewer.component';
-import { IMAGEVIEWER_CONFIG, IMAGEVIEWER_CONFIG_DEFAULT } from './imageviewer.config';
-import { HammerModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {ImageViewerComponent} from './imageviewer.component';
+import {IMAGEVIEWER_CONFIG, IMAGEVIEWER_CONFIG_DEFAULT} from './imageviewer.config';
+import {HAMMER_GESTURE_CONFIG, HammerGestureConfig, HammerModule} from '@angular/platform-browser';
+
+export class HammerConfig extends HammerGestureConfig {
+  // This configuration makes the (pan) event trigger after 1px of movement instead of 10px. It makes the UX more responsive.
+  overrides = {pan: {threshold: 1}};
+}
 
 @NgModule({
-  providers: [{ provide: IMAGEVIEWER_CONFIG, useValue: IMAGEVIEWER_CONFIG_DEFAULT }],
+  providers: [
+    { provide: IMAGEVIEWER_CONFIG, useValue: IMAGEVIEWER_CONFIG_DEFAULT },
+    {provide: HAMMER_GESTURE_CONFIG, useClass: HammerConfig}
+  ],
   imports: [HammerModule],
   declarations: [ImageViewerComponent],
   exports: [ImageViewerComponent],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,16 @@
-import {BrowserModule, HAMMER_GESTURE_CONFIG, HammerGestureConfig} from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 
-import { GettingStartedComponent } from './gettingstarted/gettingstarted.component';
-import { SharedModule } from './shared/shared.module';
-import { BasicUsageComponent } from './basicusage/basicusage.component';
-import { AutoResizeComponent } from './autoresize/autoresize.component';
-import { UploadPreviewComponent } from './uploadpreview/uploadpreview.component';
-import { ConditionalDisplayComponent } from './conditionaldisplay/conditionaldisplay.component';
-
-import { HammerModule } from '@angular/platform-browser';
-
-export class HammerConfig extends HammerGestureConfig {
-  // This configuration makes the (pan) event trigger after 1px of movement instead of 10px. It makes the UX more responsive.
-  overrides = {pan: {threshold: 1}};
-}
+import {GettingStartedComponent} from './gettingstarted/gettingstarted.component';
+import {SharedModule} from './shared/shared.module';
+import {BasicUsageComponent} from './basicusage/basicusage.component';
+import {AutoResizeComponent} from './autoresize/autoresize.component';
+import {UploadPreviewComponent} from './uploadpreview/uploadpreview.component';
+import {ConditionalDisplayComponent} from './conditionaldisplay/conditionaldisplay.component';
 
 @NgModule({
   declarations: [
@@ -33,9 +26,7 @@ export class HammerConfig extends HammerGestureConfig {
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedModule,
-    HammerModule
   ],
-  providers: [{provide: HAMMER_GESTURE_CONFIG, useClass: HammerConfig}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
